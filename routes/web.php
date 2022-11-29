63a2c25208d4247118379fb48e416868d0337985
FCastle07 <102642333+FCastle07@users.noreply.github.com>
2022-11-28 20:29:58 -0500

    Update web.php
    Agregue nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\LoginController@index') -> name('login');
 Route::get('/registro', 'App\Http\Controllers\LoginController@create');
 
-Route::view('/welcome', "welcome") -> name('welcome');
+
 Route::post('/validar-registro', 'App\Http\Controllers\LoginController@register')->name('validar-registro');
 Route::post('/inicia-sesion', 'App\Http\Controllers\LoginController@login')->name('inicia-sesion');
 Route::get('/logout', 'App\Http\Controllers\LoginController@logout')->name('logout');
 
-Route::get('/menu', 'App\Http\Controllers\MenuController@index') -> name('menu');
+
+Route::resource('productos', 'App\Http\Controllers\ProductoController')-> Middleware('auth');
+
+
+Route::get('/menu', 'App\Http\Controllers\MenuController@index')-> Middleware('auth')-> name('menu');
+Route::get('/cart', 'App\Http\Controllers\MenuController@cart')-> Middleware('auth')->name('cart.index');
+Route::post('/add', 'App\Http\Controllers\MenuController@add')->name('cart.store');
+Route::post('/update', 'App\Http\Controllers\MenuController@update')->name('cart.update');
+Route::post('/remove', 'App\Http\Controllers\MenuController@remove')->name('cart.remove');
+Route::post('/clear', 'App\Http\Controllers\MenuController@clear')->name('cart.clear'); 
